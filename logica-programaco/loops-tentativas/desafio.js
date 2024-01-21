1905649a1da84f78ad9d38999afbd56ec1220f59
Elzbieta <elzbietasousafabri@gmail.com>
2024-01-20 23:30:32 -0300

    add programação de loops e tentativas

---
+++ logica-programaco/loops-tentativas/desafio.js
@@ -0,0 +1,33 @@
+// Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 1;
+
+while (contador <= 10) {
+  console.log(contador);
+  contador++;
+}
+
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contador2 = 10;
+
+while (contador2 >= 0) {
+  console.log(contador2);
+  contador2--;
+}
+
+// Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+let numero = prompt("Digite um número:");
+let contadorRegressivo = numero;
+
+while (contadorRegressivo >= 0) {
+  console.log(contadorRegressivo);
+  contadorRegressivo--;
+}
+
+// Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+let numeroProgressivo = prompt("Digite um número:");
+let contadorProgressivo = 0;
+
+while (contadorProgressivo <= numeroProgressivo) {
+  console.log(contadorProgressivo);
+  contadorProgressivo++;
+}
